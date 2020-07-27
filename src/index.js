8bd88247cb0b0185a4da44b75d2d48fe879cdc77
Mayara Caldeira <mayaracsmith@gmail.com>
2020-07-27 10:22:55 -0400

    added api js file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const api = express();
+
+
+api.listen(3001, () => {
+  console.log('API up and running!');
+});
+
+
+api.get('/', (req, res) => {
+  console.log(req);
+  res.send('Hello, world!');
+});
