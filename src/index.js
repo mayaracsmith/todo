e54ae91e9b397e484da2784d2b96ce87bfbd9734
Mayara Caldeira <mayaracsmith@gmail.com>
2020-07-27 10:31:04 -0400

    serve todo list app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const api = express();
-
+api.use(express.static(__dirname + '/public'));
 
 api.listen(3001, () => {
   console.log('API up and running!');
 });
 
 
-api.get('/', (req, res) => {
-  console.log(req);
-  res.send('Hello, world!');
-});
+// api.get('/', (req, res) => {
+//   console.log(req);
+//   res.send('Hello, world!');
+// });
+
+
